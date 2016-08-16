b9e769ad2bdd4d11aa8faff2fc8ab0c85cd174e8
Brian Dutz and Kevin Johnston <brian@>
2016-08-16 09:29:21 -0700

    Add variables and for function in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,24 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-    
+
+    var finalNumber = parseInt($("input#numberFinal").val());
+    console.log(finalNumber);
+    var multiplier = parseInt($("input#numberMultiplier").val());
+    console.log(multiplier);
+
+    for (var results = 0; results <= finalNumber; results += multiplier) {
+      alert(results);
+    }
+
+    if (isNaN(finalNumber) || isNaN(multiplier) || finalNumber <= 0 || multiplier <= 0 || finalNumber > multiplier) {
+       $(".error").show();
+       $("#formResults").hide();
+    } else {
+      $("#formResults").show();
+      $(".error").hide();
+    }
+
   });
 
 });
